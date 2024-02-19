10c53371277731c54ca7d9b0a88816d6c96e6085
moknine <mokninemoknine999@gmail.com>
2024-02-19 15:59:06 +0100

    create our Hero component

--- app/(roote)/page.tsx
+++ app/(roote)/page.tsx
@@ -1,3 +1,43 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <main className=" ">Hello</main>;
+  return (
+    <>
+      <section className=" bg-slate-100  py-5 md:py-10">
+        <div className="wrapper grid md:grid-cols-2 grid-cols-1 gap-5 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="font-bold text-[40px] leading-[48px] lg:text-[48px] lg:leading-[50px]  xl:text-[50px] xl:leading-[74px] ">
+              Host, Connect, Celebrate: Your Events, Our Platform!
+            </h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Book and learn helpful tips from 3,168+ mentors in world-class
+              companies with our global community.
+            </p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">Explore Now</Link>
+            </Button>
+          </div>
+          <Image
+            src={"/bento.jfif"}
+            alt="hero"
+            priority
+            width={900}
+            height={900}
+            className="max-h-[600px] object-contain object-center 2xl:max-h-[50vh]"
+          />
+        </div>
+      </section>
+      <section
+        id="#events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12 "
+      >
+        <h2 className="font-bold text-[32px] leading-[40px] lg:text-[36px] lg:leading-[44px] xl:text-[40px] xl:leading-[48px]">
+          Trust by <br /> Thousands of Events
+        </h2>
+        <div className="flex flex-col w-full md:flex-row gap-6">search gap</div>
+      </section>
+    </>
+  );
 }
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <footer className="wrapper">Footer</footer>;
+  return (
+    <footer className="w-full border-t">
+      <div className="wrapper flex flex-col md:flex-row justify-between items-center gap-3">
+        <Link className="flex gap-1 items-center" href={"/"}>
+          <Image src={"logo.svg"} width={40} height={24} alt="logo" />
+          <span className="font-semibold text-xl">EventX</span>
+        </Link>
+        <p>2024 EventX. All rights reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
