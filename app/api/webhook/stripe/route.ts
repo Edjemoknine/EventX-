0a036b9df77c7e0b5c5229508bc271e3c5a4b1b1
moknine <mokninemoknine999@gmail.com>
2024-02-21 11:01:28 +0100

    commit

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,7 +1,6 @@
 import stripe from "stripe";
 import { NextResponse } from "next/server";
-import { checkoutOrder, createOrder } from "@/lib/actions/order.action";
-// import { createOrder } from "@/lib/actions/order.actions";
+import { createOrder } from "@/lib/actions/order.action";
 
 export async function POST(request: Request) {
   const body = await request.text();
